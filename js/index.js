b1555d32425a9aaa4a34fab9e007e76ffdc6cf9c
dity51028 <dipannitaofficial001@gmail.com>
2026-07-15 18:23:24 +0530

    apex chart added

---
+++ js/index.js
@@ -0,0 +1,116 @@
+var options = {
+  series: [76],
+  chart: {
+    type: 'radialBar',
+    offsetY: -20,
+    sparkline: {
+      enabled: true,
+    },
+  },
+  plotOptions: {
+    radialBar: {
+      startAngle: -90,
+      endAngle: 90,
+      track: {
+        background: '#e7e7e7',
+        strokeWidth: '97%',
+        margin: 5, // margin is in pixels
+        dropShadow: {
+          enabled: true,
+          top: 2,
+          left: 0,
+          color: '#444',
+          opacity: 1,
+          blur: 2,
+        },
+      },
+      dataLabels: {
+        name: {
+          show: false,
+        },
+        value: {
+          offsetY: -2,
+          fontSize: '22px',
+        },
+      },
+    },
+  },
+  grid: {
+    padding: {
+      top: -10,
+    },
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'light',
+      shadeIntensity: 0.4,
+      inverseColors: false,
+      opacityFrom: 1,
+      opacityTo: 1,
+      stops: [0, 50, 53, 91],
+    },
+  },
+  labels: ['Average Results'],
+}
+var options2 = {
+  series: [56],
+  chart: {
+    type: 'radialBar',
+    offsetY: -20,
+    sparkline: {
+      enabled: true,
+    },
+  },
+  plotOptions: {
+    radialBar: {
+      startAngle: -90,
+      endAngle: 90,
+      track: {
+        background: '#e7e7e7',
+        strokeWidth: '97%',
+        margin: 5, // margin is in pixels
+        dropShadow: {
+          enabled: true,
+          top: 2,
+          left: 0,
+          color: '#444',
+          opacity: 1,
+          blur: 2,
+        },
+      },
+      dataLabels: {
+        name: {
+          show: false,
+        },
+        value: {
+          offsetY: -2,
+          fontSize: '22px',
+        },
+      },
+    },
+  },
+  grid: {
+    padding: {
+      top: -10,
+    },
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'light',
+      shadeIntensity: 0.4,
+      inverseColors: false,
+      opacityFrom: 1,
+      opacityTo: 1,
+      stops: [0, 50, 53, 91],
+    },
+  },
+  labels: ['Average Results'],
+}
+
+var chart = new ApexCharts(document.querySelector('#chart'), options);
+var chart2 = new ApexCharts(document.querySelector('#chart2'), options2);
+
+chart.render();
+chart2.render();
